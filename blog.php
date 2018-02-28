a01ad8f171a5fa57e7493e6a19a38ea53b8363c4
tristanchausson <tristanchausson@me.com>
2018-02-28 12:31:59 +0100

    finalisation de la page evenement

--- blog.php
+++ blog.php
@@ -13,6 +13,17 @@
 	</nav>
 </main>
 	<h2>Blog</h2>
+
+	<form action="blog.php" method="post">
+		<p>Afficher par :</p>
+		<select name="tri" id="tri">
+			<option value="croissant">Date croissante</option>
+			<option value="decroissant">Date décroissante</option>
+		</select>
+		<button type="submit" value="filter" id="filter">Trier</button>
+	</form>
+	<br/>
+
 <?php
 	$db = mysqli_connect('localhost', 'root', 'root', 'projet_php') or die ('Erreur de connexion au serveur mySQL');
 	$query = "SELECT titre, image, intro, texte, date FROM blog ORDER BY date DESC";
@@ -22,7 +33,6 @@
 		echo $donnees['titre']."<br/>";
 		echo $donnees['intro']."<br/>";
 		echo '<img src="'.$donnees['image'].'" height="30%" width="30%">'."<br/>";
-		echo $donnees['texte']."<br/>";
 		echo $donnees['date']."<br/>"."<br/>"."<br/>";
 	}
 ?>
--- evenement.php
+++ evenement.php
@@ -14,39 +14,50 @@
 </main>
 	<h2>Evenement</h2>
 
-	<form action="contact.php" method="post">
-		<select name="thematique" id="thematique">
-			<option value="Lieu1">Trier par lieu</option>
-			<option value="Lieu1">Toulouse</option>
-			<option value="Lieu2">Bordeaux</option>
-			<option value="Lieu3">Paris</option>
-			<option value="Lieu3">Cahors</option>
-			<option value="Lieu3">Strasbourg</option>
-			<option value="Lieu3">Nantes</option>
+	<form action="evenement.php" method="post">
+		<p>Trier par lieu :</p>
+		<select name="lieu" id="lieu">
+			<option value="choix">Choix de la ville</option>
+			<option value="Toulouse">Toulouse</option>
+			<option value="Bordeaux">Bordeaux</option>
+			<option value="Paris">Paris</option>
+			<option value="Cahors">Cahors</option>
+			<option value="Strasbourg">Strasbourg</option>
+			<option value="Nantes">Nantes</option>
 		</select>
+		<button type="submit" value="filter" id="filter">Filtrer</button>
 	</form>
 	<br/>
-	<?php
-		if (empty($_SESSION['nom'])) {
-			header('Location: /login.php');
-		}
-		else {
-			$db = mysqli_connect('localhost', 'root', 'root', 'projet_php') or die ('Erreur de connexion au serveur mySQL');
-			$query = "SELECT * FROM evenement ORDER BY date DESC";
-			$result = mysqli_query($db, $query);
 
-			while ($donnees = mysqli_fetch_array($result)) {
-				echo $donnees['titre']."<br/>"."<br/>";
-				echo '<img src="'.$donnees['image'].'" height="30%" width="30%">'."<br/>";
-				echo $donnees['intro']."<br/>"."<br/>";
-				echo 'Débute le : '.$donnees['date']."<br/>";
-				echo 'Et se termine le : '.$donnees['fin']."<br/>"."<br/>";
-				echo 'À : '.$donnees['lieu']."<br/>";
-				echo 'Publié le : '.$donnees['publi']."<br/>"."<br/>"."<br/>";
-			}
-		}
+<?php
+	$db = mysqli_connect('localhost', 'root', 'root', 'projet_php') or die ('Erreur de connexion au serveur mySQL');
+	
+	if (empty($_SESSION['nom'])) {
+		header('Location: /login.php');
+	}
 
-	?>
+	else if (isset($_POST['lieu']) && ($_POST['lieu']) !== "choix") {
+		$lieu = $_POST['lieu'];
+		$query = "SELECT * FROM evenement WHERE lieu = '".$lieu."' ORDER BY date DESC";
+	}
+
+	else {
+		$query = "SELECT * FROM evenement ORDER BY date DESC";
+	}
+
+	$result = mysqli_query($db, $query);
+
+	while ($donnees = mysqli_fetch_array($result)) {
+		echo $donnees['titre']."<br/>"."<br/>";
+		echo '<img src="'.$donnees['image'].'" height="30%" width="30%">'."<br/>";
+		echo $donnees['intro']."<br/>"."<br/>";
+		echo 'Débute le : '.$donnees['date']."<br/>";
+		echo 'Et se termine le : '.$donnees['fin']."<br/>"."<br/>";
+		echo 'À : '.$donnees['lieu']."<br/>";
+		echo 'Publié le : '.$donnees['publi']."<br/>"."<br/>"."<br/>";
+	}
+
+?>
 
 
 
